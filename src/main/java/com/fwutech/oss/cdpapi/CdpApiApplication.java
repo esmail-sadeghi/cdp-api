954aa0aad8e9aba7b2ae6f6c0c1230649c94c212
esmaeilsadeghijob <esmaeilsadeghijob@gmail.com>
2026-09-12 19:11:12 +0330

    first commit

---
+++ src/main/java/com/fwutech/oss/cdpapi/CdpApiApplication.java
@@ -0,0 +1,13 @@
+package com.fwutech.oss.cdpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fwutech/oss/cdpapi/CdpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fwutech.oss.cdpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
